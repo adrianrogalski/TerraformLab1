8bcfdc223938a688e71e7e430a6a932a89a1d2c8
Adrian Rogalski <adrianrogalski99@gmail.com>
2025-10-24 19:09:22 +0200

    create terraform infra

---
+++ main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_version = ">1.3.7"
+  required_providers {
+    azurerm = {
+        source = "hashicorp/azurerm"
+        version = "~>3.43.0"
+    }
+  }
+
+    cloud {    
+        organization = "adrianrogalski" 
+        workspaces { 
+            name = "terraform-test-1" 
+    } 
+  } 
+} 
+
+provider "azurerm" {
+  features {
+    
+  }
+}
+
+resource "azurerm_resource_group" "rg" {
+  name = "ar-resource-group"
+  location = "Central US"
+}
+
+resource "azurerm_storage_account" "storage" {
+  name = "arstrg101"
+  location = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  account_tier = "Standard"
+  account_replication_type = "RAGRS"
+}
